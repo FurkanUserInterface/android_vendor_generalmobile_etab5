bd61960eb79de100b042ac6894b0c23444a1f3f4
kutlay1653 <kutlaysarimehmet@gmail.com>
2017-05-03 18:00:36 +0300

    Adding Files

---
+++ etab5-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/GeneralMobile/etab5/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/GeneralMobile/etab5/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/GeneralMobile/etab5/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/GeneralMobile/etab5/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/GeneralMobile/etab5/proprietary/vendor,system/vendor)
